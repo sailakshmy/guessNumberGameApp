1cbe841deb4aa34569b118e58a43a1972c79d773
sailakshmy <sailakshmy94@gmail.com>
2025-03-06 22:40:57 +0400

    Created new components and added styling for the StartGameScreen component

---
+++ guessGameApp/screens/StartGameScreen.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: "#72063c",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
+
+export default StartGameScreen;
