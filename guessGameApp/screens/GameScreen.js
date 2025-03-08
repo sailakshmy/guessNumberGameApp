652312cd8f2bd528af1c9a0b4d0ba85c73e0513e
sailakshmy <sailakshmy94@gmail.com>
2025-03-08 17:48:25 +0400

    Handled the computer guess logic

--- guessGameApp/screens/GameScreen.js
+++ guessGameApp/screens/GameScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
 
@@ -13,15 +14,52 @@ function generateRandomBetween(min, max, exclude) {
     return rndNum;
   }
 }
+let minBoundary = 1;
+let maxBoundary = 100;
 
 const GameScreen = ({ userChoice }) => {
   const [currentGuess, setCurrentGuess] = useState(
-    generateRandomBetween(1, 100, userChoice)
+    generateRandomBetween(minBoundary, maxBoundary, userChoice)
   );
+
+  const generateNextGuess = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < userChoice) ||
+      (direction === "higher" && currentGuess > userChoice)
+    ) {
+      Alert.alert("Don't lie", "You know that this is wrong....", [
+        { text: "Sorry", style: "cancel" },
+      ]);
+      return;
+    }
+    if (direction === "lower") {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    console.log(minBoundary, maxBoundary);
+    const newGuess = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+    setCurrentGuess(newGuess);
+  };
   return (
     <View style={styles.screenContainer}>
       <Title>Opponent's Guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
+      <View>
+        <Text>Higher Or Lower</Text>
+        <View>
+          <PrimaryButton onPress={generateNextGuess.bind(this, "higher")}>
+            +
+          </PrimaryButton>
+          <PrimaryButton onPress={generateNextGuess.bind(this, "lower")}>
+            -
+          </PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
