6a2427f6dc52521c240d5f6afe1984857dc7150e
sailakshmy <sailakshmy94@gmail.com>
2025-03-11 23:32:22 +0400

    Updated the UI to handle different orientation related styles

--- guessGameApp/screens/GameScreen.js
+++ guessGameApp/screens/GameScreen.js
@@ -1,6 +1,12 @@
 import { Ionicons } from "@expo/vector-icons";
 import React, { useEffect, useState } from "react";
-import { Alert, FlatList, StyleSheet, View } from "react-native";
+import {
+  Alert,
+  FlatList,
+  StyleSheet,
+  View,
+  useWindowDimensions,
+} from "react-native";
 import GuessListItem from "../components/game/GuessListItem";
 import NumberContainer from "../components/game/NumberContainer";
 import Card from "../components/ui/Card";
@@ -25,6 +31,8 @@ const GameScreen = ({ userChoice, onGameOverEvent }) => {
   const initialGuess = generateRandomBetween(1, 100, userChoice);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
   const [guessRounds, setGuessRounds] = useState([initialGuess]);
+
+  const { width, height } = useWindowDimensions();
   const generateNextGuess = (direction) => {
     if (
       (direction === "lower" && currentGuess < userChoice) ||
@@ -61,9 +69,9 @@ const GameScreen = ({ userChoice, onGameOverEvent }) => {
   }, []);
 
   const guessRoundsLength = guessRounds?.length;
-  return (
-    <View style={styles.screenContainer}>
-      <Title>Opponent's Guess</Title>
+
+  let content = (
+    <>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card>
         <Instruction styling={styles.customisedInstructionText}>
@@ -82,6 +90,31 @@ const GameScreen = ({ userChoice, onGameOverEvent }) => {
           </View>
         </View>
       </Card>
+    </>
+  );
+  if (width > 500) {
+    content = (
+      <>
+        <View style={styles.buttonsContainerHorizontal}>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={generateNextGuess.bind(this, "higher")}>
+              <Ionicons name="add" size={24} color={"white"} />
+            </PrimaryButton>
+          </View>
+          <NumberContainer>{currentGuess}</NumberContainer>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={generateNextGuess.bind(this, "lower")}>
+              <Ionicons name="remove" size={24} color={"white"} />
+            </PrimaryButton>
+          </View>
+        </View>
+      </>
+    );
+  }
+  return (
+    <View style={styles.screenContainer}>
+      <Title>Opponent's Guess</Title>
+      {content}
       <View style={styles.listContainer}>
         {/* {guessRounds?.map((guessRound) => (
           <Text key={guessRound}>{guessRound}</Text>
@@ -128,6 +161,10 @@ const styles = StyleSheet.create({
   listContainer: {
     flex: 1,
     padding: 16,
+  },
+  buttonsContainerHorizontal: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
 
